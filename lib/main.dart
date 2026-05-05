6e51a4cfcb170f225aa74446f52baca5708810f3
machadev <macha4126@gmail.com>
2026-05-05 20:54:29 +0900

    Firebase接続追加

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'providers/collection_provider.dart';
 import 'screens/main_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const CarNumberApp());
 }
 
